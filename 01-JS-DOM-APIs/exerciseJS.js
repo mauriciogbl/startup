2db1458da74306bfe5a33c926e4e7ab3b0083585
Mauricio Miño <mauricio.mino@globant.com>
2016-10-11 17:56:38 -0300

    Topic1 - Mauricio Miño - Ejercicio 8 finalizado, ejercicio 9 en desarrollo

--- 01-JS-DOM-APIs/exerciseJS.js
+++ 01-JS-DOM-APIs/exerciseJS.js
@@ -1,31 +1,43 @@
-//   function eventRequest() {
-//     var xhttp = new XMLHttpRequest();
-//     xhttp.open('GET', 'http://api.icndb.com/jokes/random', true);
-//     xhttp.onreadystatechange = function(event) {
-//       var response = JSON.parse(event.target.response);
-//       document.getElementById('sectionHidden').innerHTML = response.value.joke;
-//        if (xhttp.readyState === XMLHttpRequest.DONE) {
-//          if (xhttp.responseText.value) {
-//              document.getElementById("buttonSection").innerHTML  = xhttp.responseText.value.joke;
-//          }
-//        }
-//    }
-//    xhttp.send();
-// }
+//EXERCISE 6
+  function eventRequest() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('GET', 'http://api.icndb.com/jokes/random', true);
+    xhttp.onreadystatechange = function(event) {
+      var response = JSON.parse(event.target.response);
+      document.getElementById('sectionHidden').innerHTML = response.value.joke;
+       if (xhttp.readyState === XMLHttpRequest.DONE) {
+         if (xhttp.responseText.value) {
+             document.getElementById("buttonSection").innerHTML  = xhttp.responseText.value.joke;
+         }
+       }
+   }
+   xhttp.send();
+}
+//EXERCISE 7
 function eventOnClick(){
   var config = {
     url: 'http://api.icndb.com/jokes/random'
 };
   function resolve(xhttp) {
-      var response = JSON.parse(event.target.response);
-      document.getElementById('sectionHidden').innerHTML = response.value.joke;
-      if (xhttp.readyState === XMLHttpRequest.DONE)
-        {
-          if (xhttp.responseText.value)
-            {
-              document.getElementById("buttonSection").innerHTML  = xhttp.responseText.value.joke;
-            }
-        }
+    console.log(xhttp.responseText);
+    var response = JSON.parse(event.target.response);
+    document.getElementById('sectionHidden').innerHTML = response.value.joke;
+};
+  function reject() {
+    alert('Something went wrong !');
+    document.getElementById("sectionHidden").style.color = "red";
+};
+  eventRequestReusable(config).then(resolve,reject);
+};
+//EXERCISE 9
+function secondEventOnClick(){
+  var config = {
+    url: 'https://api.github.com/search/repositories'
+};
+  function resolve(xhttp) {
+    console.log(xhttp.responseText);
+    var response = JSON.parse(event.target.response);
+    document.getElementById('sectionHidden').innerHTML = response.errors[0].field;
 };
   function reject() {
     alert('Something went wrong !');
@@ -34,13 +46,14 @@ function eventOnClick(){
   eventRequestReusable(config).then(resolve,reject);
 
 };
+//EXERCISE 7 AND 9
 function eventRequestReusable(object, resolve, reject) {
   return new Promise( function (resolve, reject) {
   var xhttp = new XMLHttpRequest();
   xhttp.open('GET', object.url , true);
   xhttp.send();
   xhttp.onload = function () {
-    if (this.status != 200) {
+    if (this.status == 200 || this.status == 422) {
       resolve(xhttp);
     } else {
       reject();
@@ -52,3 +65,11 @@ function eventRequestReusable(object, resolve, reject) {
   };
 })
 };
+
+// if (xhttp.readyState === XMLHttpRequest.DONE)
+//   {
+//     if (xhttp.responseText.value)
+//       {
+//         document.getElementById("buttonSection").innerHTML  = xhttp.responseText.value.joke;
+//       }
+//   }
